7f2eca5096b2a6c729784fdf8da2fcfc1d6b59aa
wusatosi <26424577+wusatosi@users.noreply.github.com>
2026-01-04 20:29:07 -0800

    move all infra files other than CI

--- infra/cmake/gnu-toolchain.cmake
+++ infra/cmake/gnu-toolchain.cmake
@@ -3,25 +3,24 @@
 # This toolchain file is not meant to be used directly,
 # but to be invoked by CMake preset and GitHub CI.
 #
-# This toolchain file configures for apple clang family of compiler.
-# Note this is different from LLVM toolchain.
+# This toolchain file configures for GNU family of compiler.
 #
 # BEMAN_BUILDSYS_SANITIZER:
 # This optional CMake parameter is not meant for public use and is subject to
 # change.
 # Possible values:
-# - MaxSan: configures clang and clang++ to use all available non-conflicting
-#           sanitizers. Note that apple clang does not support leak sanitizer.
-# - TSan:   configures clang and clang++ to enable the use of thread sanitizer.
+# - MaxSan: configures gcc and g++ to use all available non-conflicting
+#           sanitizers.
+# - TSan:   configures gcc and g++ to enable the use of thread sanitizer
 
 include_guard(GLOBAL)
 
-set(CMAKE_C_COMPILER clang)
-set(CMAKE_CXX_COMPILER clang++)
+set(CMAKE_C_COMPILER gcc)
+set(CMAKE_CXX_COMPILER g++)
 
 if(BEMAN_BUILDSYS_SANITIZER STREQUAL "MaxSan")
     set(SANITIZER_FLAGS
-        "-fsanitize=address -fsanitize=pointer-compare -fsanitize=pointer-subtract -fsanitize=undefined"
+        "-fsanitize=address -fsanitize=leak -fsanitize=pointer-compare -fsanitize=pointer-subtract -fsanitize=undefined -fsanitize-undefined-trap-on-error"
     )
 elseif(BEMAN_BUILDSYS_SANITIZER STREQUAL "TSan")
     set(SANITIZER_FLAGS "-fsanitize=thread")
@@ -37,3 +36,6 @@ set(CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT "${RELEASE_FLAGS}")
 
 set(CMAKE_C_FLAGS_RELEASE_INIT "${RELEASE_FLAGS}")
 set(CMAKE_CXX_FLAGS_RELEASE_INIT "${RELEASE_FLAGS}")
+
+# Add this dir to the module path so that `find_package(beman-install-library)` works
+list(APPEND CMAKE_PREFIX_PATH "${CMAKE_CURRENT_LIST_DIR}")
