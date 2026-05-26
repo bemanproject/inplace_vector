d40140cdf80d8812a3c32b222fb599876ffe4b6a
Oliver Deng <oliver.t.deng@gmail.com>
2026-05-26 11:22:59 -0400

    fix: remove unncessary gtest dependency

--- tests/beman/inplace_vector/compare.test.cpp
+++ tests/beman/inplace_vector/compare.test.cpp
@@ -330,7 +330,7 @@ TEST(Compare, threeway_uncomparable) {
 
 // In accordance with P3698R0, we compare vectors with identical elements but
 // different capacities
-TEST(Compare, threeway_cross_capacity_smaller) {
+TEST(Compare, threeway_cross_capacity) {
   vec_list<inplace_vector<int, 4>, inplace_vector<int, 5>> list{
       .empty{},
       .base{1, 2, 3},
